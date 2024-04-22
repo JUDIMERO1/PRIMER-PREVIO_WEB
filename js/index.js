bf4b3bc9d2123182f64bc81e54e5928e873c7360
JUDIMERO1 <131618404+JUDIMERO1@users.noreply.github.com>
2024-04-22 10:56:45 -0500

    comit index.js

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+const formulario = document.getElementById('login');
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const codigoEstudiante = document.querySelector("#code").value;
+    const claveEstudiante = document.querySelector("#password").value;
+
+    if (codigoEstudiante === '' || claveEstudiante === '') {
+        console.log= 'Por favor, ingrese todas las credenciales.';
+        return;
+    }
+
+    loginUsuario(codigoEstudiante, claveEstudiante);
+});
+
+function loginUsuario(codigoEstudiante, claveEstudiante) {
+    const url = 'https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/login';
+    const data = {
+        codigo: codigoEstudiante,
+        clave: claveEstudiante
+    };
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(user => {
+            if (user.login) {
+                console.log('Login exitoso:', user.mensaje);
+                localStorage.setItem('usuario', JSON.stringify(user));
+                alert('Bienvenido ' + user.nombre);
+                window.location.href = "pages/notas.html";
+            } else {
+                alert(user.mensaje);
+            }
+        })
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+        });
+}
